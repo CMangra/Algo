3bd1739484312693080c3a037c9b3b86a195eb34
Chaitanya <schlockmangra@gmail.com>
2022-07-07 00:21:24 +0200

    8_6

---
+++ 8/8_6.py
@@ -0,0 +1,42 @@
+"""
+Aufgabe:
+Do exercise P-6.34 (evaluation of postfix notation) in Goodrich et al. Hint: use a stack.
+
+Aufgabe im Buch:
+Implement a program that can input an expression in postfix notation (see Exercise C-6.22) and output its value.
+"""
+
+
+def post_fix(expression):
+    split_expression = expression.split()
+    print(split_expression)
+
+    stack = []
+    result = None
+
+    for element in split_expression:
+        if element in "0123456789":
+            stack.append(float(element))
+        elif element == "-":
+            number2 = stack.pop()
+            result = stack.pop() - number2
+        elif element == "+":
+            result = stack.pop() + stack.pop()
+        elif element == "*":
+            result = stack.pop() * stack.pop()
+        elif element == "/":
+            number2 = stack.pop()
+            result = stack.pop() / number2
+
+        if not (result == None):
+            stack.append(result)
+            result = None
+
+    return stack
+
+
+print(post_fix("6 8 / 5 /"))
+print(post_fix("8 7 2 / - 3 + 4 -"))
+print(post_fix("8 5 + 9 4 / - "))
+print(post_fix("8 7 3 * 4 * - 6 +"))
+print(post_fix("4 5 9 / 6 * + 7 - "))
