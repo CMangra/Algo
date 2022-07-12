0a4f2c22686bd7f0b76ab7a7c83f248906615861
Chaitanya.Mangra <chaitanya.mangra@smartinsurtech.de>
2022-07-13 00:15:45 +0200

    first attempt at 11.4

--- 11/11.4.py
+++ 11/11.4.py
@@ -5,3 +5,47 @@ Create a simple decision tree composed of binary attributes for a game of your c
 to evaluate it based on the values of the attributes concerned.
 
 """
+
+
+class Node:
+    def __init__(self, data):
+        self.left = None
+        self.right = None
+        self.data = data
+
+    def insert(self, data):
+        if self.data:
+            if data < self.data:
+                if self.left is None:
+                    self.left = Node(data)
+                else:
+                    self.left.insert(data)
+            elif data > self.data:
+                if self.right is None:
+                    self.right = Node(data)
+                else:
+                    self.right.insert(data)
+            else:
+                self.data = data
+
+    def inorder(self):
+        if self.left:
+            self.left.inorder()
+        print(self.data)
+        if self.right:
+            self.right.inorder()
+
+
+def visible_audible_10mAway_flank(S):
+    assert len(S) == 4
+    root = Node(S[0])
+    root.left = Node(S[1])
+    root.right = Node
+    root.right = Node
+
+if __name__ == '__main__':
+    #1101
+    root = Node(0)
+    root.left = Node(1)
+    root.left.left = Node('Creep')
+    root.right = Node
